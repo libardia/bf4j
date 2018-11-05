377759eaa31d52ecc2099978d0cfb54cb19e227a
Anthony Libardi <libardi42@gmail.com>
2018-11-04 21:36:31 -0600

    Added opper as a dependency

--- src/main/java/info/tonyl/bf4j/Bf4jApp.java
+++ src/main/java/info/tonyl/bf4j/Bf4jApp.java
@@ -1,14 +1,6 @@
 package info.tonyl.bf4j;
 
-import info.tonyl.bf4j.lang.Minimizer;
-
 public class Bf4jApp {
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			System.out.println("Input:     " + args[0]);
-			System.out.println("Minimized: " + Minimizer.minimize(args[0]));
-		} else {
-			System.out.println("No input.");
-		}
 	}
 }
---
+++ src/main/java/info/tonyl/bf4j/options/Options.java
@@ -0,0 +1,6 @@
+package info.tonyl.bf4j.options;
+
+public class Options {
+	public static final String FILE = "f";
+	public static final String FILE_LONG = "file";
+}

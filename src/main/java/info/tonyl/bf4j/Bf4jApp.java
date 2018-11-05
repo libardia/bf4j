6b9698275281c737a1121d09e9d90f55c1cbafe3
Anthony Libardi <libardi42@gmail.com>
2018-11-04 22:46:14 -0600

    Builds the dependency in. Need to change the name.

--- src/main/java/info/tonyl/bf4j/Bf4jApp.java
+++ src/main/java/info/tonyl/bf4j/Bf4jApp.java
@@ -1,6 +1,11 @@
 package info.tonyl.bf4j;
 
+import info.tonyl.bf4j.options.Options;
+import info.tonyl.opper.Opper;
+
 public class Bf4jApp {
 	public static void main(String[] args) {
+		Options.registerAndParse(args);
+		System.out.println(Opper.tempGetOptions());
 	}
 }
--- src/main/java/info/tonyl/bf4j/options/Options.java
+++ src/main/java/info/tonyl/bf4j/options/Options.java
@@ -1,6 +1,20 @@
 package info.tonyl.bf4j.options;
 
+import info.tonyl.opper.Opper;
+
 public class Options {
 	public static final String FILE = "f";
 	public static final String FILE_LONG = "file";
+	public static final String INPUT = "i";
+	public static final String SCRIPT = "s";
+	public static final String SHOW_MINIMIZED = "m";
+	public static final String DEBUG = "D";
+	
+	public static void registerAndParse(String[] args) {
+		Opper.register(new String[] { Options.FILE, Options.FILE_LONG });
+		Opper.register(Options.INPUT, true);
+		Opper.register(Options.SCRIPT, true);
+		Opper.register(Options.SHOW_MINIMIZED, Options.DEBUG);
+		Opper.parse(args);
+	}
 }
